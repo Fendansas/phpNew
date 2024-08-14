7efdef9e09c5e5aafa99260ef2a71329cbe6e6a1
sergey <fendansas@gmai.com>
2024-08-14 23:09:07 +0300

    done with constant start operators

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
     <li><a href="/php/syntax-basics.php">Основы синтаксисаs</a></li>
     <li><a href="/php/types.php">Типы</a></li>
     <li><a href="/php/variables.php">Variables</a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
+    <li><a href="/php/constant.php">Constant</a></li>
+    <li><a href="/php/operators.php">Operators</a></li>
     <li><a href=""></a></li>
     <li><a href=""></a></li>
 </ul>
